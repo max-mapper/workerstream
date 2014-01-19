4c341e669bb782615f73fe56bab069b4c7b10e76
aaron <a.aa.akio.aaron@gmail.com>
2014-01-19 09:46:38 -0800

    Cleanup - Remove unused 'me' reference in index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var stream = require('stream')
 var util = require('util')
 
 function WorkerStream(path) {
-  var me = this
-  stream.Stream.call(me)
+  stream.Stream.call(this)
   this.readable = true
   this.writable = true
-  me.worker = typeof path === 'string'
+  this.worker = typeof path === 'string'
     ? new Worker(path)
     : path
-  me.worker.onmessage = me.workerMessage.bind(this)
-  me.worker.onerror = me.workerError.bind(this)
+  this.worker.onmessage = this.workerMessage.bind(this)
+  this.worker.onerror = this.workerError.bind(this)
 }
 
 util.inherits(WorkerStream, stream.Stream)
