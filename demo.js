165862ada3a116bdfa596752e8a0e9321aa7ba8a
Max Ogden <max@maxogden.com>
2014-03-18 16:17:35 -0700

    make demo use a relative require from #7

--- demo.js
+++ demo.js
@@ -1,11 +1,11 @@
-var workerstream = require('./index');
+var workerstream = require('./index')
 
-var worker = workerstream('demo-worker.js');
+var worker = workerstream('demo-worker.js')
 
 worker.on('data', function(data) {
-  console.log(new Uint8Array(data));
-});
+  console.log(new Uint8Array(data))
+})
 
-var ab = new ArrayBuffer( 10 );
-worker.write( ab );
-worker.end();
+var ab = new ArrayBuffer( 10 )
+worker.write( ab )
+worker.end()
